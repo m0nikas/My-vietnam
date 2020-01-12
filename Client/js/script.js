e65433a32a72c8aa0ce43c8d6b12e87db239b285
m0nikas <monika.s2016@vitstudent.ac.in>
2020-01-12 13:47:42 +0530

    few changes

--- Client/js/script.js
+++ Client/js/script.js
@@ -1,10 +1,34 @@
-function getAlerted(){
-	var person1 = {name:'Eldor', age: 28, location: 'San Francisco'};
-	var person2 = {name: 'Medina', age: 21, location: 'Las Vegas'};
-	var person3 = {name: 'Alex', age: 25, location: 'Los Angeles'};
+var days=[{
+	name:"Northern Vietnam Package Tour",
+	way:"Hanoi - Halong Bay - Ninh Binh",
+	price:"50,000",
+	duration:5,
+	itinery:"Day 1Hanoi Arrival Day 2 Hanoi - Halong Bay Day 3 Halong Bay - Hanoi City Tour Day 4 Hanoi - Ninh Binh - Hoa Lu - Tam Coc Day 5 Hanoi - Departure Activities Hanoi arrival, hotel check-in Depart for Halong, embark on the overnight cruise Leaving Halong, come back to Hanoi Transfer to Ninh Binh, visit Hoa Lu ancient citadel, take a sampan in Tam Coc Free at your leisure, Depart for hometown",
+    image:"https://www.vietnampackagedeals.com/images/Indi-Maps/northern-vietnam-package-tour.jpg"
 
-	var people = [person1,person2,person3];
-	for(var i = 0; i < people.length; i++){
-		document.getElementById('demo').innerHTML += people[i].name + '<br>';
-	}
-}
+},
+{
+	name:"Fantastic Ho Chi Minh - Hanoi - Halong Bay Tour",
+	way:"Ho Chi Minh - Hanoi - Halong Bay",
+	price:"70,000",
+	duration:6,
+	itinery:"Day 1 Ho Chi Minh arrival Day 2 Ho Chi Minh City - Cu Chi Tunnels Day 3 Ho Chi Minh City - Hanoi Day 4 Hanoi City Tour Day 5 Hanoi - Halong Bay Day 6 Hanoi - Departure Activities Ho Chi Minh arrival, hotel check-in Depart for Cu Chi Tunnels, Ho Chi Minh city tour Depart Ho Chi Minh City, Fly to Hanoi Hanoi city Exploration Further exploration on Bay, transfer back to Hanoi, Hanoi Sightseeing Tour Depart Vietnam",
+    image:"https://www.vietnampackagedeals.com/images/Indi-Maps/ho-chi-minh-hanoi-halong-bay-tour.jpg"
+
+},
+{
+	name:"Great Vietnam Honeymoon Beach Holiday",
+	way:"Hanoi - Halong Bay - Phu Quoc Island - Ho Chi Minh",
+	price:"1,50,000",
+	duration:8,
+	itinery:"Day 1 Hanoi arrival Day 2 Hanoi Sightseeing Day 3 Hanoi - Halong Bay Day 4 Halong Bay - Hanoi  Day 5 Hanoi - Phu Quoc Island Day 6 Phu Quoc Island - Free dayDay 7 Phu Quoc Island - Free day Day 8 Phu Quoc Island - Ho Chi Minh City Departure",
+    image:"https://www.vietnampackagedeals.com/images/Indi-Maps/vietnam-honeymoon-beach-holiday.jpg"
+},
+{
+	name:"Best Vietnam and Cambodia Tour",
+	way:"Siem Reap - Hanoi - Halong Bay - Hanoi",
+	price:"80,000",
+	duration:6,
+	itinery:" Day 1 Siem Reap Arrival - Angkor Wat Day 2 Siem Reap - Angkor Thom - Banteay Srey Day 3 Siem Reap - Tonle Sap Lake - Hanoi Day 4 Hanoi - Halong Bay Day 5 Halong Bay - Hanoi City Tour Day 6 Hanoi - Departure",
+	image:"https://www.vietnampackagedeals.com/images/Indi-Maps/best-vietnam-cambodia-tour.jpg"
+}]
